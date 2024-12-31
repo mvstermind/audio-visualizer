d5fbaab1ef7c9eda36d53eb8774e0de1ca12a8d7
mvstermind <zablockidawid1@gmail.com>
2024-12-31 12:16:20 +0100

    feat: all mp3's are turnin into wav files

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from errors import InvalidFileFormatError
 import numpy as np
 from scipy.io import wavfile
 import os
@@ -10,27 +9,36 @@ def main():
 
     print(files)
 
-    # for file in files:
-    #     rate, _data = wavfile.read(file)
-    #     print(f"{file} has {rate} sample rate")
-
 
 # reads all fiels, looks for .mp3 and .wav
 # converts all .mp3 to .wav with ffmpeg
 # return a list of all wav files
 def read_audio_db(path_name: str) -> list[str]:
     wav_files: list[str] = []
+
+    # first turn all files into wav
     pathlist = Path(path_name).rglob("*")
     for path in pathlist:
-        if str(path).endswith(".mp3") or str(path).endswith(".wav"):
-            convert_to_wav(str(path))
-            wav_files.append(str(path))
+        # is it how i supposed to handle spaces in filenames??
+        if " " in rf"{str(path)}":
+            path_no_space: str = str(path).replace(" ", "\\ ")
+            convert_to_wav(path_no_space)
 
+        else:
+            convert_to_wav(str(path))
+
+    new_path = Path(path_name).rglob("*")
+    for path in new_path:
+        if str(path).endswith(".wav"):
+            wav_files.append(str(path))
+        else:
+            continue
     return wav_files
 
 
 # dunno what's better way to make it work
 def convert_to_wav(filename: str) -> None:
+    """converts ONLY mp3 files to wav using ffmpeg"""
     if filename.endswith(".wav"):
         return
 
@@ -41,7 +49,7 @@ def convert_to_wav(filename: str) -> None:
         return
 
     else:
-        raise InvalidFileFormatError("use valid file format")
+        pass
 
 
 def remove_mp3(filename: str) -> None:
